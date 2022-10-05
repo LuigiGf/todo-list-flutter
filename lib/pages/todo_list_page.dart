da9c9a7bff688db03dd7524dbc87696c25675836
Luigi Gottardello Fonseca <luigi.gottardello.fonseca@gmail.com>
2022-10-04 21:54:36 -0300

    feat: add clear button style

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -9,29 +9,54 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Add new task',
-                    hintText: 'Ex: study Flutter.',
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Add new task',
+                        hintText: 'Ex: study Flutter.',
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xff00d7f3),
+                        padding: EdgeInsets.all(19)),
+                    child: Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  )
+                ],
               ),
               SizedBox(
-                width: 8,
+                height: 16,
               ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xff00d7f3),
-                    padding: EdgeInsets.all(19)),
-                child: Icon(
-                  Icons.add,
-                  size: 30,
-                ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'You have 0 tasks pendents',
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xff00d7f3),
+                        padding: EdgeInsets.all(19)),
+                    child: Text('Clear all'),
+                  )
+                ],
               )
             ],
           ),
