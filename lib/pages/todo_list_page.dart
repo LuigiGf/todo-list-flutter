883266b406b9bc7b5a2e3757667619edb8df5abf
Luigi Gottardello Fonseca <luigi.gottardello.fonseca@gmail.com>
2022-10-04 21:48:49 -0300

    feat: add input and button widget

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -5,8 +5,38 @@ class TodoListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Add new task',
+                    hintText: 'Ex: study Flutter.',
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff00d7f3),
+                    padding: EdgeInsets.all(19)),
+                child: Icon(
+                  Icons.add,
+                  size: 30,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
